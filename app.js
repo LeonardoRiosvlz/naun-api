e61ecf5128e1ed00e1aa11bc1400cf9a5a8f8083
leonardo <leonardo27188@gmail.com>
2021-05-27 18:53:25 -0400

    ultimo intento

--- app.js
+++ app.js
@@ -11,16 +11,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
+// database
+const db = require("./models");
+const Role = db.role;
+db.sequelize.sync().then(() => {
+ //initial(); // Just use it in development, at the first time execution!. Delete it in production
+});
+
 
 app.use('/public',express.static(`${__dirname}/storage/imgs`));
-
+app.use('/api', require('./routes/user.routes'));
+app.use('/api', require('./routes/auth.routes'));
 
 // Middleware para Vue.js router modo history
 const history = require('connect-history-api-fallback');
 app.use(history());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('puerto', process.env.PORT || 4000);
+app.set('puerto', process.env.PORT || 5000);
 app.listen(app.get('puerto'), () => {
   console.log('Example app listening on port'+ app.get('puerto'));
 });
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,50 +1,31 @@
+import express from 'express';
+const router = express.Router();
 const { authJwt } = require("../middlewares");
 const controller = require("../controllers/user.controller");
 const upload = require('../libs/storage');
 const cpUpload = upload.fields([{ name: 'filename', maxCount: 1 },{ name: 'firma', maxCount: 1 }])
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-
-    next();
-  });
-
   // Retrieve all Books
-  app.get("/user/all",[authJwt.verifyToken], controller.findAll);
+  router.get("/user/all",[authJwt.verifyToken], controller.findAll);
 
-  app.get("/api/user/contactos",[authJwt.verifyToken], controller.findAllContacto);
+  router.get("/api/user/contactos",[authJwt.verifyToken], controller.findAllContacto);
 
-  app.get("/user/coordinadores",[authJwt.verifyToken], controller.findCoordinadores);
+  router.get("/user/coordinadores",[authJwt.verifyToken], controller.findCoordinadores);
   
-  app.get("/user/pefil",[authJwt.verifyToken], controller.findOne);
+  router.get("/user/pefil",[authJwt.verifyToken], controller.findOne);
 
-  app.post("/api/user/tecnico",[cpUpload,authJwt.verifyToken], controller.findTecnico);
+  router.post("/api/user/tecnico",[cpUpload,authJwt.verifyToken], controller.findTecnico);
 
-  app.post("/api/user/regional",[cpUpload,authJwt.verifyToken], controller.findregional);
+  router.post("/api/user/regional",[cpUpload,authJwt.verifyToken], controller.findregional);
 
-  app.get("/api/user/adminstrador",[cpUpload,authJwt.verifyToken], controller.findAdministrador);
+  router.get("/api/user/adminstrador",[cpUpload,authJwt.verifyToken], controller.findAdministrador);
   
-  app.put("/user/pefil",[cpUpload ,authJwt.verifyToken], controller.update);
+  router.put("/user/pefil",[cpUpload ,authJwt.verifyToken], controller.update);
 
-  app.put("/api/user/canal",[cpUpload ,authJwt.verifyToken], controller.updateCanal);
+  router.put("/api/user/canal",[cpUpload ,authJwt.verifyToken], controller.updateCanal);
   
-  app.get("/api/test/all", controller.allAccess);
+  router.get("/api/test/all", controller.allAccess);
 
-  app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
+  router.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
 
-  app.get(
-    "/api/test/mod",
-    [authJwt.verifyToken, authJwt.isModerator],
-    controller.moderatorBoard
-  );
-
-  app.get(
-    "/api/test/admin",
-    [authJwt.verifyToken, authJwt.isAdmin],
-    controller.adminBoard
-  );
-};
+  module.exports = router;
