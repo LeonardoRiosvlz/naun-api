38b4e49dc57ee74d436e5f69b6ef9a1e6195d38f
Leonardo Rios <62246248+LeonardoRiosvlz@users.noreply.github.com>
2021-07-01 16:52:12 -0400

    Update vdocumento.routes.js

--- routes/vdocumento.routes.js
+++ routes/vdocumento.routes.js
@@ -2,45 +2,28 @@ import express from 'express';
 const router = express.Router();
 const { authJwt } = require("../middlewares");
 const { verifySignUp } = require("../middlewares");
-const Controller = require("../controllers/documentos.controller.js");
+const Controller = require("../controllers/vdocumentos.controller.js");
 const upload = require('../libs/storage');
 const cpUpload = upload.fields([{ name: 'filename', maxCount: 1 },{ name: 'diagrama', maxCount: 1 }])
 
 
 
-  
+
     // Create a new cargo
-    router.post("/documentos",[cpUpload,authJwt.verifyToken], Controller.create);
-  
+    router.post("/documentos/versiones",[cpUpload,authJwt.verifyToken], Controller.create);
+
+
     // Create a new cargo
-    router.post("/documentos/listar",[cpUpload,authJwt.verifyToken], Controller.listarAdmin);
-  
-    // Create a new cargo
-    router.post("/documentos/find",[cpUpload,authJwt.verifyToken,authJwt.isModeratorOrAdmin], Controller.find);
+    router.post("/documentos/versiones/find",[cpUpload,authJwt.verifyToken,authJwt.isModeratorOrAdmin], Controller.find);
     // Update a cargo with id
 
-    router.put("/documentos/editar",[cpUpload,authJwt.verifyToken, authJwt.isModeratorOrAdmin], Controller.updateDocument);
-  
-    // Retrieve all cargos
-    router.get("/documentos",authJwt.verifyToken, Controller.findAll);
-  
     // Update a cargo with id
-    router.put("/documentos",[cpUpload,authJwt.verifyToken], Controller.update);
-  
+    router.put("/documentos/versiones",[cpUpload,authJwt.verifyToken], Controller.update);
+
     // Delete a cargo with id
     router.post("/documentos/delete",[cpUpload,authJwt.verifyToken], Controller.delete);
 
-  
-    // Create a new cargo
-    router.post("/documentos/version",[cpUpload,authJwt.verifyToken], Controller.createVersion);
 
 
-    router.put("/documentos/version/editar",[cpUpload,authJwt.verifyToken, authJwt.isModeratorOrAdmin], Controller.updateVersion);
-
-    // Delete a cargo with id
-    router.post("/documentos/version/delete",[cpUpload,authJwt.verifyToken], Controller.deleteVersion);
-    
-    // Create a new cargo
-    router.post("/documentos/versiones/find",[cpUpload,authJwt.verifyToken,authJwt.isModeratorOrAdmin], Controller.findVersion);
 
     module.exports = router;
